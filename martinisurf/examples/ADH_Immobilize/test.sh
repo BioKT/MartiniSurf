3fc0438e5229f1980f934fb5900f9b27501e8a6f
Juan Jimenez <juanjimenezg@unc.edu.ar>
2025-12-02 19:04:00 +0100

    new logo

--- martinisurf/examples/ADH_Immobilize/test.sh
+++ martinisurf/examples/ADH_Immobilize/test.sh
@@ -1,6 +1,5 @@
-martinisurf --pdb 1RJW.pdb --moltype Protein --lx 20 --ly 20 --surface-bead C1 --resA 8 10 11 --resB 1025 1027 1028 --dist 10 --merge A,B,C,D
+martinisurf --pdb 1RJW.pdb --moltype Protein --lx 20 --ly 20 --surface-bead C1 --anchor 1 8 10 11 --anchor 2 1025 1027 1028 --dist 10 --merge A,B,C,D
 
 ### Using surface previous generate
 
-martinisurf --pdb 1RJW.pdb --moltype Protein --surface surface.gro --resA 8 10 11 --resB 1025 1027 1028 --dist 10 --merge A,B,C,D
-
+martinisurf --pdb 1RJW.pdb --moltype Protein --surface surface.gro --anchor 1 8 10 11 --anchor 2 1025 1027 1028 --dist 10 --merge A,B,C,D
